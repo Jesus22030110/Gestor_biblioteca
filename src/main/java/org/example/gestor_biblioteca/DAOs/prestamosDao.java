2c493b5427230c07bd6d531400308f785533eff6
Jesus22030110 <22030110@icelaya.edu.mx>
2025-05-22 02:04:55 -0600

    modelos listos (ahora si)

--- src/main/java/org/example/gestor_biblioteca/DAOs/prestamosDao.java
+++ src/main/java/org/example/gestor_biblioteca/DAOs/prestamosDao.java
@@ -1,18 +1,18 @@
 package org.example.gestor_biblioteca.DAOs;
 
-import java.util.date;
+import java.util.Date;
 
 public class prestamosDao {
     private int id_prestamo;
     private int id_usuario;
     private int id_libro;
-    private date fecha_prestamo;
-    private date fecha_limite_prestamo;
-    private date fecha_devolucion;
+    private Date fecha_prestamo;
+    private Date fecha_limite_prestamo;
+    private Date fecha_devolucion;
 
     public prestamosDao() {}
 
-    public prestamosDao(int id_prestamo, int id_usuario, int id_libro, date fecha_prestamo, date fecha_limite_prestamo, date fecha_devolucion){
+    public prestamosDao(int id_prestamo, int id_usuario, int id_libro, Date fecha_prestamo, Date fecha_limite_prestamo, Date fecha_devolucion){
         this.id_prestamo = id_prestamo;
         this.id_usuario = id_usuario;
         this.id_libro = id_libro;
@@ -45,27 +45,27 @@ public class prestamosDao {
         this.id_libro = id_libro;
     }
 
-    public date getFecha_prestamo() {
+    public Date getFecha_prestamo() {
         return fecha_prestamo;
     }
 
-    public void setFecha_prestamo(date fecha_prestamo) {
+    public void setFecha_prestamo(Date fecha_prestamo) {
         this.fecha_prestamo = fecha_prestamo;
     }
 
-    public date getFecha_limite_prestamo() {
+    public Date getFecha_limite_prestamo() {
         return fecha_limite_prestamo;
     }
 
-    public void setFecha_limite_prestamo(date fecha_limite_prestamo) {
+    public void setFecha_limite_prestamo(Date fecha_limite_prestamo) {
         this.fecha_limite_prestamo = fecha_limite_prestamo;
     }
 
-    public date getFecha_devolucion() {
+    public Date getFecha_devolucion() {
         return fecha_devolucion;
     }
 
-    public void setFecha_devolucion(date fecha_devolucion) {
+    public void setFecha_devolucion(Date fecha_devolucion) {
         this.fecha_devolucion = fecha_devolucion;
     }
 }
